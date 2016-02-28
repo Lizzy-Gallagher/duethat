ec58a3b9ede52f14d1a2c7e3347be15cdf8af886
mlchow <marisalchow@gmail.com>
2016-02-28 06:00:51 -0500

    added prelim flask integration with google doc

--- webapp/duethat/duethat/static/index.js
+++ webapp/duethat/duethat/static/index.js
@@ -1,15 +1,30 @@
 var base_url = "https://api.projectoxford.ai/luis/v1/application?id=a500a19e-f7ab-4078-9f6c-9b6b8ee3bbeb&subscription-key=598c92029bc24dff8b713c669d774773&q="
 
 
-var classify = function() {
-	var text = document.getElementById("query").value;
-	console.log(text);
+var classify = function(text) {
+	//var text = document.getElementById("query").value;
+	//console.log(text);
 
 	var full_url = base_url + text;
 	$.getJSON(full_url, function(data) {
 		// do something
-		showResults(data)
+		getResult(data)
 	});
+}
+
+var getResult = function(data) {
+	// data = the JSON from the LUIS AI
+	// create objects and append to appendDiv
+	var appendDiv = document.getElementById("duenote-objs");
+	var newObj = document.createElement("div");
+
+	var intent = data["intents"][0]["intent"];
+	var entities = data["entities"];
+	newObj.innerHTML = intent;
+	for (i = 0; i < entities.length; i++) {
+		newObj.innerHTML += "\n" + entities[i]["entity"];
+	}
+	appendDiv.appendChild(newObj);
 }
 
 var showResults = function(data) {
@@ -36,9 +51,16 @@ var showResults = function(data) {
 
 }
 
-var clear = function() {
-	var result_box = document.getElementById("result-box");
-	while (result_box.firstChild) {
-	    result_box.removeChild(result_box.firstChild);
+var clear = function(node-name) {
+	var node = document.getElementById(node-name);
+	while (node.firstChild) {
+	    node.removeChild(node.firstChild);
 	}
+}
+
+
+var selectDoc = function(num) {
+	var duenote_base_url = "/duenote="
+	var url = duenote_base_url + num
+	window.open(url, "_self");
 }
--- webapp/duethat/duethat/views.py
+++ webapp/duethat/duethat/views.py
@@ -1,15 +1,20 @@
 from duethat import app
 from flask import render_template, request
 
+import search
+import json
+
 from GoogleCalendar import GoogleCalendar
 from GoogleTask import GoogleTask
 from GoogleDoc import GoogleDoc
-import cal
+
+documents = GoogleDoc()
+list_of_readable_files = documents.get_list_of_readable_files()
 
 # homepage, landing page
 @app.route('/')
 def index():
-	documents = GoogleDoc()
+	#documents = GoogleDoc()
 	list_of_readable_files = documents.get_list_of_readable_files()
 
 	filenames = []
@@ -17,10 +22,14 @@ def index():
 	for file in list_of_readable_files:
 		filenames.append(file['name'])
 
-	return render_template('index.html')
+	return render_template('index2.html', filesnames=filenames)
 
-# page that shows docs
-@app.route('/duethat')
-def duethat():
-	return render_template('duethat.html')
+# where you show the tasks and events gathere
+@app.route('/duenote=<index>')
+def duenote(index):
+	target = list_of_readable_files[index]
+	text = documents.get_text_from_file(target)
 
+	textSnippets = search(text)
+
+	return render_template('duenote.html', textSnippets=textSnippets)
